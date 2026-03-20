93a37415ed9cd5e7070c671ce93a27e0e445850a
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-03-20 17:13:05 -0300

    nova update

--- usuarios.php
+++ usuarios.php
@@ -3,9 +3,80 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/52ff4c741b.js" crossorigin="anonymous"></script>
     <title>Usuários</title>
+    <style>
+        *{
+            margin: 0px;
+            padding: 0px;
+        }
+
+        h1{
+            margin: 10px;
+        }
+
+        #table-container{
+            border-radius: 5px;
+            margin: 10px;
+            overflow: hidden;
+        }
+
+        table{
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        thead{
+            background-color: #2e3e50;
+            color: white;
+        }
+
+        td, th{
+            font-size: .8em;
+            padding: 10px;
+            text-align: center;
+        }
+
+        @media screen and (min-width: 800px) {
+            td, th{
+                font-size: 1em;
+            }
+        }
+    </style>
 </head>
 <body>
-    
+    <h1>Gestão de usuários</h1>
+    <div id="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nome</th>
+                    <th>E-mail</th>
+                    <th>Acesso</th>
+                    <th>Status</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>001</td>
+                    <td>Pedro Giacciani</td>
+                    <td>giaccianipedro@gmail.com</td>
+                    <td>Administrador</td>
+                    <td>Ativo</td>
+                    <td><button><i class="fa-solid fa-pen"></i></button></td>
+                </tr>
+                <tr>
+                    <td>002</td>
+                    <td>Eric Freitas</td>
+                    <td>eric@unifev.edu</td>
+                    <td>Usuário</td>
+                    <td>Ativo</td>
+                    <td><button><i class="fa-solid fa-pen"></i></button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
