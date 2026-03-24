9d1df2066a1511e056714936413533718fe7376c
PedroGiacciani <giaccianipedro@gmail.com>
2026-03-23 22:37:46 -0300

    Update

--- modulo.php
+++ modulo.php
@@ -7,12 +7,46 @@
     <title>Módulo de Usuários</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="tabelas.css">
+    <style>
+        #tableh h1{
+            margin: 10px;
+        }
+
+        #tableh{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between; 
+            align-items: center;
+            margin: 10px;
+        }
+
+        #tableh  button{
+            border-radius: 5px;
+            border: 1px solid black;
+            padding: 10px;
+            background-color: #2743c0;
+            color: white;
+            cursor: pointer;
+            transition-duration: .2s;
+        }
+
+        #tableh  button:hover{
+            background-color: #5272ff;
+        }
+
+        #tableh i{
+            margin-right: 5px;
+        }
+    </style>
 </head>
 <body>
     <div class="sep">
         <header><?php include("menu.php");?></header>
         <main>
-            <h1>Gestão de usuários</h1>
+            <div id="tableh">
+                <h1>Gestão de usuários</h1>
+                <a href="usuarios.php"><button class="add"><i class="fa-solid fa-user-plus"></i> Adicionar usuário</button></a>
+            </div>
             <div id="table-container">
                 <table>
                     <thead>
@@ -59,11 +93,6 @@
                             </td>
                         </tr>
                     </tbody>
-                    <tfoot>
-                        <tr>
-                            <td colspan="6" id="btn-add"><i class="fa-solid fa-plus"></i> <a href="usuarios.php">Adicionar usuário</a></td>
-                        </tr>
-                    </tfoot>
                 </table>
             </div>
         </main>
--- postagens.php
+++ postagens.php
@@ -7,12 +7,46 @@
     <title>Postagens</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="tabelas.css">
+    <style>
+        #tableh h1{
+            margin: 10px;
+        }
+
+        #tableh{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between; 
+            align-items: center;
+            margin: 10px;
+        }
+
+        #tableh  button{
+            border-radius: 5px;
+            border: 1px solid black;
+            padding: 10px;
+            background-color: #2743c0;
+            color: white;
+            cursor: pointer;
+            transition-duration: .2s;
+        }
+
+        #tableh  button:hover{
+            background-color: #5272ff;
+        }
+
+        #tableh i{
+            margin-right: 5px;
+        }
+    </style>
 </head>
 <body>
     <div class="sep">
         <header><?php include("menu.php");?></header>
         <main>
-            <h1>Gestão de postagens</h1>
+            <div id="tableh">
+                <h1>Gestão de postagens</h1>
+                <a href="add-postagens.php"><button class="add"><i class="fa-solid fa-user-plus"></i> Adicionar postagem</button></a>
+            </div>
             <div id="table-container">
                 <table>
                     <thead>
